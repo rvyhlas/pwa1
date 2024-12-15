a094a71e25a3ab26b83ccdcad3894b45f4b0dde6
rvyhlas <35924129+rvyhlas@users.noreply.github.com>
2024-12-15 16:13:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,20 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(registration => {
-                console.log('Service Worker registrace úspěšná:', registration);
-            })
-            .catch(error => {
-                console.log('Service Worker registrace neúspěšná:', error);
-            });
-    });
-}
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-pwa-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/harvestor.jpg'
+            ]);
+        })
+    );
+});
 
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
